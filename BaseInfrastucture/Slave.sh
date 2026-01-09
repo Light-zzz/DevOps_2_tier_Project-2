bcbcdbc017fc2dcb6b3d68d06d544e5eaf363aef
Dipesh Vishwakarma <dipeshvishwa767@gmail.com>
2026-01-09 20:14:07 +0530

    Update Slave.sh

--- BaseInfrastucture/Slave.sh
+++ BaseInfrastucture/Slave.sh
@@ -4,3 +4,17 @@ set -e
 # Update and install Java (Jenkins requires Java 11 or 17)
 sudo apt update -y
 sudo apt install -y fontconfig openjdk-17-jdk wget gnupg
+
+# Check Java Version
+java --version
+
+# Create Directory to store Slave Deployments
+mkdir -p /home/ubuntu/FlaskWebDeploy
+
+#Check Directory Permissions
+ls -ld /home/ubuntu/FlaskWebDeploy
+
+# Give Required permissions
+sudo chown -R ubuntu:ubuntu /home/ubuntu/FlaskWebDeploy
+chmod 755 /home/ubuntu/FlaskWebDeploy
+chown ubuntu:ubuntu /home/ubuntu/FlaskWebDeploy
